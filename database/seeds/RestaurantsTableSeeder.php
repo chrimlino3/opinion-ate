89f04ea0879395368c34be2cba93b2d56e994f42
ChrisMeliniotou <chris.melinioti@gmail.com>
2020-01-18 14:35:08 +0100

    json:api set up

---
+++ database/seeds/RestaurantsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Restaurant;
+
+class RestaurantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sushiPlace = Restaurant::create(['name' => 'Sushi Place', 'address' => '123 Main Street']); // we can pass the attributes to the ::create() method in an array
+        $burgerPlace = Restaurant::create(['name' => 'Burger Place', 'address' => '456 Other Street']);
+
+        $sushiPlace->dishes()->createMany([          // We can access the dishes() relationship for a given restaurant.
+            ['name'=> 'Volcano Roll', 'rating' => 3],
+            ['name' => 'Salmon Nigiri', 'rating' => 4],
+        ]);
+
+        $burgerPlace->dishes()->createMany([                //createMany() records on that relationship.
+            ['name' => 'Barbeque Burger', 'rating' => '5'],
+            ['name' => 'Slider', 'rating' => 3],
+        ]);
+    }
+}
